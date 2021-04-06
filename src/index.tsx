2dca19f0c49b7bfaf335ea170b8033a8a2c11a0a
Jemal Mohamedngus <jemalmngus@gmail.com>
2021-04-06 12:04:28 +0300

    initial commit

---
+++ src/index.tsx
@@ -0,0 +1,9 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+import { App } from "./App";
+
+
+ReactDOM.render(
+  <App userName="jemal" lang="TypeScript" />,
+  document.getElementById("output")
+);
