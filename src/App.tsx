14b55b40b7a0f81d0970b5603e803169f2923bd2
leulhabte <leulh63@gmail.com>
2021-04-07 10:27:43 -0300

    ✨ NEW: adds global theme - global theme added

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
-import React, {lazy, Suspense} from 'react';
+import React, { lazy, Suspense } from 'react';
 import AppBar from "./layouts/applayout/appbar";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
-import { makeStyles, Theme, createStyles } from '@material-ui/core/styles';
+import { makeStyles, Theme, createStyles, ThemeProvider } from '@material-ui/core/styles';
 import { LinearProgress } from '@material-ui/core';
+import { theme } from "./app/theme"
 
 
-const RegisterUser = lazy(()=> import("./Pages/signup/registration"))
+
+const RegisterUser = lazy(() => import("./Pages/signup/registration"))
 
 const drawerWidth = 240;
 
@@ -48,17 +50,19 @@ export const App = () => {
   const classes = useStyles();
   return (
     <div className={classes.root}>
-      <Suspense fallback={<LinearProgress color="primary" />}>
-      <BrowserRouter>
-      <AppBar/>
-      <main className={classes.content}>
-        <div className={classes.toolbar} />
-          <Switch>
-            <Route path="/" component={RegisterUser} exact/>
-          </Switch>
-      </main>
-      </BrowserRouter>
-      </Suspense>
+      <ThemeProvider theme={theme}>
+        <Suspense fallback={<LinearProgress color="primary" />}>
+          <BrowserRouter>
+            <AppBar />
+            <main className={classes.content}>
+              <div className={classes.toolbar} />
+              <Switch>
+                <Route path="/" component={RegisterUser} exact />
+              </Switch>
+            </main>
+          </BrowserRouter>
+        </Suspense>
+      </ThemeProvider>
     </div>
   );
 }
---
+++ src/app/theme/index.ts
@@ -0,0 +1,15 @@
+import { createMuiTheme } from "@material-ui/core";
+
+export const theme = createMuiTheme({
+    palette:{
+        type: 'light',
+        primary:{
+            main: "#F0F0F0",
+            light: "#C5B235",
+            dark: "#F0F0F0",
+        },
+        secondary:{
+            main: "#2F3A45",
+        }
+    }
+})
