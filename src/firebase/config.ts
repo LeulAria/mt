c7146a7ba25c4eeafff5075725d39b1c66bd9ca3
leulhabte <leulh63@gmail.com>
2021-04-07 04:53:25 -0300

    📦 NEW:  adds firebase to project - add and initialize firebase

---
+++ src/firebase/config.ts
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+export const firebaseConfig = {
+    apiKey: "AIzaSyC7A9C-8_5FApAxJwjarGwD3NhY77pSHsM",
+    authDomain: "user-management-ee9c6.firebaseapp.com",
+    databaseURL: "https://user-management-ee9c6-default-rtdb.firebaseio.com",
+    projectId: "user-management-ee9c6",
+    storageBucket: "user-management-ee9c6.appspot.com",
+    messagingSenderId: "434505675842",
+    appId: "1:434505675842:web:814cb23eb9b5ee21b8a9db",
+    measurementId: "G-3NQ4WJJM6S"
+  };
