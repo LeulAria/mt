f8bc86e15eeb042229547369f42dbee73545a939
leulhabte <leulh63@gmail.com>
2021-04-07 04:04:34 -0300

    📦 NEW:  adds global store to the app - global store added

---
+++ src/app/store/index.ts
@@ -0,0 +1,14 @@
+import {configureStore, ThunkAction, Action, getDefaultMiddleware} from '@reduxjs/toolkit';
+
+export const store = configureStore({
+    reducer:{}
+})
+
+export type RootState = ReturnType<typeof store.getState>;
+
+export type AppThunk<ReturnType = void> = ThunkAction<
+	ReturnType,
+	RootState,
+	unknown,
+	Action<string>
+>;
