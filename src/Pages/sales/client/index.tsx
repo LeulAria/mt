35804b28ba8bb5e22bdc52f73d0c4a6b9ad86029
Helina G <helinagirmay@gmail.com>
2021-04-17 11:11:19 +0300

    👌 IMPROVE: edit on client list page

--- src/Pages/sales/client/index.tsx
+++ src/Pages/sales/client/index.tsx
@@ -4,29 +4,26 @@ import TextField from "@material-ui/core/TextField";
 import Grid from "@material-ui/core/Grid";
 import EditIcon from "@material-ui/icons/Edit";
 import { useDispatch, useSelector } from "react-redux";
-import firebase from "../../../firebase/firebase";
 import Button from "@material-ui/core/Button";
 import Dialog from "@material-ui/core/Dialog";
-import DialogContent from "@material-ui/core/DialogContent";
-import DialogTitle from "@material-ui/core/DialogTitle";
-import Slide from "@material-ui/core/Slide";
-import { TransitionProps } from "@material-ui/core/transitions";
 import {
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogTitle,
   Divider,
   FormControl,
   IconButton,
-  InputAdornment,
-  InputLabel,
-  MenuItem,
-  OutlinedInput,
-  Select,
+  Tooltip,
 } from "@material-ui/core";
 import { getUser, sendVerification } from "../../../features/auth/actions";
 import { AppThunk, RootState } from "../../../app/store";
 import { makeStyles, useTheme } from "@material-ui/core/styles";
+import VisibilityIcon from "@material-ui/icons/Visibility";
 import { Chip } from "@material-ui/core";
 import { useForm, Controller } from "react-hook-form";
 import PersonAddDisabledIcon from "@material-ui/icons/PersonAddDisabled";
+import EditClient from "./editClient";
 
 const useStyles = makeStyles((theme) => ({
   formControl: {
@@ -36,19 +33,15 @@ const useStyles = makeStyles((theme) => ({
     margin: theme.spacing(3, 0, 2),
   },
 }));
-const Transition = React.forwardRef(function Transition(
-  props: TransitionProps & { children?: React.ReactElement<any, any> },
-  ref: React.Ref<unknown>
-) {
-  return <Slide direction="up" ref={ref} {...props} />;
-});
 
 export default function User() {
   const { control, handleSubmit } = useForm();
   const [stat, getStatus] = useState("");
   const classes = useStyles();
   const [rowData, getRowData] = useState([]);
+  const [index, getIndex] = useState(0);
   const [open, setOpen] = React.useState(false);
+  const [openView, setOpenView] = React.useState(false);
   const dispatch = useDispatch();
 
   const handleClickOpen = () => {
@@ -59,17 +52,16 @@ export default function User() {
     setOpen(false);
   };
 
+  const handleSuspend = (u: any) => {
+    console.log(u, "----");
+    u.flaged = true;
+    setOpen(false);
+  };
+
   const columns = [
     {
       label: "Company Name",
       name: "companyName",
-      options: {
-        filter: true,
-      },
-    },
-    {
-      label: "Company URL",
-      name: "companyUrl",
       options: {
         filter: true,
       },
@@ -88,21 +80,6 @@ export default function User() {
       options: {
         filter: true,
         sort: false,
-      },
-    },
-    {
-      label: "Role",
-      name: "role",
-      options: {
-        filter: true,
-        sort: false,
-      },
-    },
-    {
-      label: "Service",
-      name: "service",
-      options: {
-        filter: true,
       },
     },
     {
@@ -126,58 +103,38 @@ export default function User() {
               size="small"
               label={stat ? stat : "loading"}
               color="secondary"
-              // onClick={handleClick}
+              onClick={handleClickOpen}
             />
           );
         },
       },
     },
     {
-      label: "Payment Status",
-      name: "paymentStat",
-      options: {
-        filter: true,
-        sort: false,
-        customBodyRenderLite: function custom(dataIndex: any, rowIndex: any) {
-          return (
-            <Chip
-              variant="outlined"
-              size="small"
-              label={stat ? stat : "pending"}
-              color="secondary"
-              // onClick={handleClick}
-            />
-          );
-        },
-      },
-    },
-    {
-      name: "Edit",
+      label: "Options",
+      name: "",
       options: {
         filter: false,
         sort: false,
         empty: true,
         customBodyRenderLite: function custom(dataIndex: any, rowIndex: any) {
           return (
-            <IconButton onClick={handleClickOpen}>
-              <EditIcon />
-            </IconButton>
-          );
-        },
-      },
-    },
-    {
-      label: "Suspend User",
-      name: "suspend",
-      options: {
-        filter: false,
-        sort: false,
-        empty: true,
-        customBodyRenderLite: function custom(dataIndex: any, rowIndex: any) {
-          return (
-            <IconButton>
-              <PersonAddDisabledIcon />
-            </IconButton>
+            <>
+              <Tooltip title="Edit Client">
+                <IconButton onClick={handleClickOpen}>
+                  <EditIcon />
+                </IconButton>
+              </Tooltip>
+              <Tooltip title="View Detail">
+                <IconButton>
+                  <VisibilityIcon />
+                </IconButton>
+              </Tooltip>
+              <Tooltip title="Suspend User">
+                <IconButton onClick={handleClickOpen}>
+                  <PersonAddDisabledIcon />
+                </IconButton>
+              </Tooltip>
+            </>
           );
         },
       },
@@ -188,37 +145,17 @@ export default function User() {
     filter: true,
     onRowClick: (event: any, rowData: any) => {
       console.log("eventtts", event, rowData, typeof rowData);
+      getIndex(event);
       getRowData((rowData = event));
     },
   };
 
   const stateClient = useSelector((state: RootState) => state.auth);
-  console.log(stateClient.clients, "--cli");
+  console.log(stateClient.clients, rowData, "--cli");
 
   useEffect(() => {
     dispatch(getUser());
   }, []);
-
-  const onSubmit = (data: any) => {
-    data.email = rowData[2];
-
-    dispatch(sendVerification(data));
-
-    alert(JSON.stringify(data));
-    const updatedUser = {
-      ...data,
-    };
-    const db = firebase.firestore();
-    db.collection("clients")
-      .doc(rowData[6])
-      .update(updatedUser)
-      .then((_) => {
-        console.log("");
-      });
-    stateClient.clients;
-    getStatus(data.verification_status);
-    console.log(getStatus, stat, "0000");
-  };
 
   return (
     <div
@@ -235,180 +172,26 @@ export default function User() {
 
       <Dialog
         open={open}
-        TransitionComponent={Transition}
-        keepMounted
         onClose={handleClose}
-        aria-labelledby="alert-dialog-slide-title"
-        aria-describedby="alert-dialog-slide-description"
+        // PaperComponent={PaperComponent}
+        aria-labelledby="draggable-dialog-title"
       >
-        <DialogTitle id="alert-dialog-slide-title">
-          {"Edit Client Information"}
+        <DialogTitle style={{ cursor: "move" }} id="draggable-dialog-title">
+          Subscribe
         </DialogTitle>
-        <Divider />
-        <Divider />
         <DialogContent>
-          <Grid container spacing={2}>
-            <Grid item xs={12} sm={6}>
-              <label>Company Name: </label>
-
-              <TextField
-                required
-                value={rowData[0]}
-                disabled
-                variant="outlined"
-                placeholder="Company Name"
-                fullWidth
-              />
-            </Grid>
-
-            <Grid item xs={12} sm={6}>
-              <label>Email: </label>
-
-              <TextField
-                required
-                id="email"
-                name="email"
-                value={rowData[2]}
-                disabled
-                variant="outlined"
-                placeholder="Email"
-                fullWidth
-              />
-            </Grid>
-          </Grid>
-          <br />
-          <Divider />
-          <Divider />
-          <br />
-          <form noValidate onSubmit={handleSubmit(onSubmit)}>
-            <Grid container spacing={2}>
-              {/* <Grid container direction='row' spacing={1}> */}
-
-              <Grid item xs={12} sm={6}>
-                <Controller
-                  render={({ field }) => (
-                    <TextField
-                      required
-                      id="country"
-                      {...field}
-                      placeholder="Country"
-                      variant="outlined"
-                      fullWidth
-                    />
-                  )}
-                  name="country"
-                  control={control}
-                />
-              </Grid>
-              <Grid item xs={12} sm={6}>
-                <Controller
-                  render={({ field }) => (
-                    <TextField
-                      required
-                      id="city"
-                      {...field}
-                      placeholder="City"
-                      variant="outlined"
-                      fullWidth
-                    />
-                  )}
-                  name="city"
-                  control={control}
-                />
-              </Grid>
-              <Grid item xs={12} sm={6}>
-                <Controller
-                  render={({ field }) => (
-                    <TextField
-                      required
-                      id="subCity"
-                      {...field}
-                      placeholder="Sub-City"
-                      variant="outlined"
-                      fullWidth
-                    />
-                  )}
-                  name="subCity"
-                  control={control}
-                />
-              </Grid>
-              {/* </Grid> */}
-
-              {/* <label>Payment</label> */}
-              <Grid item xs={12} sm={6}>
-                <Controller
-                  render={({ field }) => (
-                    <TextField
-                      required
-                      id="paymentStat"
-                      {...field}
-                      placeholder="Payment Status"
-                      variant="outlined"
-                      fullWidth
-                    />
-                  )}
-                  name="paymentStat"
-                  control={control}
-                />
-              </Grid>
-
-              <Grid item xs={12} sm={6}>
-                <Controller
-                  render={({ field }) => (
-                    <TextField
-                      required
-                      id="tinNumber"
-                      {...field}
-                      placeholder="Tin Number"
-                      variant="outlined"
-                      fullWidth
-                    />
-                  )}
-                  name="tinNumber"
-                  control={control}
-                />
-              </Grid>
-              <Grid item xs={12} sm={6}>
-                <Controller
-                  render={({ field }) => (
-                    <FormControl
-                      variant="outlined"
-                      className={classes.formControl}
-                    >
-                      <InputLabel id="demo-simple-select-outlined-label">
-                        Verification Status
-                      </InputLabel>
-                      <Select
-                        style={{ backgroundColor: "#fff" }}
-                        {...field}
-                        labelId="demo-simple-select-outlined-label"
-                        id="demo-simple-select-outlined"
-                        label="Gender"
-                      >
-                        <MenuItem value={"NOT_VERIFIED"}>Not Verified</MenuItem>
-                        <MenuItem value={"PENDING"}>Pending</MenuItem>
-                        <MenuItem value={"VERIFIED"}>Verified</MenuItem>
-                      </Select>
-                    </FormControl>
-                  )}
-                  name="verification_status"
-                  control={control}
-                  defaultValue="NOT_VERIFIED"
-                />
-              </Grid>
-
-              <Button
-                type="submit"
-                fullWidth
-                variant="contained"
-                color="secondary"
-                className={classes.submit}
-              >
-                Update
-              </Button>
-            </Grid>
-          </form>
+          <DialogContentText>
+            Are you sure you want to suspend user {rowData[0]}?
+          </DialogContentText>
         </DialogContent>
+        <DialogActions>
+          <Button autoFocus onClick={handleClose} color="primary">
+            Cancel
+          </Button>
+          <Button onClick={() => handleSuspend(rowData)} color="primary">
+            Suspend
+          </Button>
+        </DialogActions>
       </Dialog>
     </div>
   );
